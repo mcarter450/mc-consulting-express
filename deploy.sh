7c1d852897a5f5dbe39923508d46227ef2a6b63a
Michael Carter <mikeaaroncarter@gmail.com>
2023-01-19 23:18:27 -0800

    Add exclude list for deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-rsync -avz --exclude '.env' --exclude 'node_modules' ./ ec2-user@lightsail:/var/www/express-server/
+rsync -avz --exclude-from='exclude-list.txt' ./ ec2-user@lightsail:/var/www/express-server/
